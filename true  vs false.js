cb8003b175bad72d3d88e88bc0260da385a44faa
rashed876 <you@example.com>
2021-02-02 15:59:35 +0600

    true false

---
+++ true  vs false.js
@@ -0,0 +1,21 @@
+// falsy:
+// false
+// 0
+// ""
+// udenfined
+// null 
+// NaN
+
+// Truthy:
+// "0" 
+// " "
+// {}
+// []
+
+let name = [];
+if(Name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
